b247a903350ab967cdc315bbde9de1ae29f3f838
lukashecke <57808594+lukashecke@users.noreply.github.com>
2022-02-20 19:31:08 +0100

    Ungewolltes Löschen der Datenbank verhindern

--- resources/Sql/CreateDB.sql
+++ resources/Sql/CreateDB.sql
@@ -10,7 +10,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Database
 -- -----------------------------------------------------
-DROP DATABASE IF EXISTS Firestocks;
+-- DROP DATABASE IF EXISTS Firestocks;
 CREATE DATABASE Firestocks CHARACTER SET utf8;
 
 USE Firestocks;
