a4a36b54702cf7480e30b7b3c5fe4131ab91433c
lukashecke <57808594+lukashecke@users.noreply.github.com>
2022-03-31 11:37:00 +0200

    Automatische Aktualisierung der   Gesamtinvestition

--- src/Controllers/PortfolioController.java
+++ src/Controllers/PortfolioController.java
@@ -1,44 +1,17 @@
 package Controllers;
 
-import Models.Asset;
 import Models.PortfolioModel;
 import Views.PortfolioWindow;
-
-import javax.swing.*;
-import java.util.Collection;
-
-import static java.util.stream.Collectors.toList;
 
 public class PortfolioController {
     private PortfolioModel model;
     private PortfolioWindow view;
 
-    private Collection<? extends Asset> investedAssets;
-
     public PortfolioController(PortfolioModel portfolioModel, PortfolioWindow portfolioWindow) {
         this.model = portfolioModel;
         this.view = portfolioWindow;
 
-        investedAssets = model.getInvestedAssets();
-
-        updateView();
-    }
-
-    public void updateView() {
-        DefaultListModel<Asset> etfListModel = new DefaultListModel<>();
-        etfListModel.addAll(investedAssets.stream().filter(t -> t.getType().getId() == 1).collect(toList()));
-        view.getInvestedETFs().setModel(etfListModel);
-        view.getInvestedETFs().repaint();
-
-        DefaultListModel<Asset> cryptoListModel = new DefaultListModel<>();
-        cryptoListModel.addAll(investedAssets.stream().filter(t -> t.getType().getId() == 2).collect(toList()));
-        view.getInvestedCrypto().setModel(cryptoListModel);
-        view.getInvestedCrypto().repaint();
-
-        DefaultListModel<Asset> metalLstModel = new DefaultListModel<>();
-        metalLstModel.addAll(investedAssets.stream().filter(t -> t.getType().getId() == 3).collect(toList()));
-        view.getInvestedMetals().setModel(metalLstModel);
-        view.getInvestedMetals().repaint();
+        this.view.refresh(model.getInvestedAssets());
     }
 
     public void showWindow(String title, int width, int height) {
--- src/Views/PortfolioWindow.java
+++ src/Views/PortfolioWindow.java
@@ -2,15 +2,22 @@ package Views;
 
 import Business.DBConnection;
 import Controls.AssetList;
+import Models.Asset;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Collection;
+
+import static java.util.stream.Collectors.toList;
 
 public class PortfolioWindow extends BaseWindow {
     private Container container;
 
     JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP,JTabbedPane.SCROLL_TAB_LAYOUT);
     JPanel infoPanel = new JPanel();
+    JLabel totalSum = new JLabel();
 
     private AssetList investedETFs = new AssetList();
     private AssetList investedCrypto = new AssetList();
@@ -30,7 +37,8 @@ public class PortfolioWindow extends BaseWindow {
 
         infoPanel.setLayout(new BorderLayout());
         infoPanel.add(new JLabel("Gesamtinvestition:"), BorderLayout.WEST);
-        infoPanel.add(new JLabel(DBConnection.getInstance().getTotalInvestment()), BorderLayout.CENTER);
+        totalSum.setText(DBConnection.getInstance().getTotalInvestment());
+        infoPanel.add(totalSum, BorderLayout.CENTER);
         infoPanel.add(new JLabel("+ 50 € (2)"), BorderLayout.EAST);
         container.add(infoPanel, BorderLayout.NORTH);
 
@@ -40,6 +48,14 @@ public class PortfolioWindow extends BaseWindow {
         button.addActionListener((e) -> {
             var window = new AddInvestmentWindow();
             window.showWindow("Neues Investment",750,550);
+            window.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    refresh(DBConnection.getInstance().GetInvestedAssets());
+                    revalidate();
+                    repaint();
+                }
+            });
         });
         buttonPane.add(button, BorderLayout.EAST);
         buttonPane.setBackground(Color.white);
@@ -56,5 +72,27 @@ public class PortfolioWindow extends BaseWindow {
 
     public AssetList getInvestedMetals() {
         return investedMetals;
+    }
+
+    public void refresh(Collection<? extends Asset> investedAssets) {
+        // Tabs
+        DefaultListModel<Asset> etfListModel = new DefaultListModel<>();
+        etfListModel.addAll(investedAssets.stream().filter(t -> t.getType().getId() == 1).collect(toList()));
+        this.getInvestedETFs().setModel(etfListModel);
+        this.getInvestedETFs().repaint();
+
+        DefaultListModel<Asset> cryptoListModel = new DefaultListModel<>();
+        cryptoListModel.addAll(investedAssets.stream().filter(t -> t.getType().getId() == 2).collect(toList()));
+        this.getInvestedCrypto().setModel(cryptoListModel);
+        this.getInvestedCrypto().repaint();
+
+        DefaultListModel<Asset> metalLstModel = new DefaultListModel<>();
+        metalLstModel.addAll(investedAssets.stream().filter(t -> t.getType().getId() == 3).collect(toList()));
+        this.getInvestedMetals().setModel(metalLstModel);
+        this.getInvestedMetals().repaint();
+
+        // Übersicht
+        this.totalSum.setText(DBConnection.getInstance().getTotalInvestment());
+        this.totalSum.repaint();
     }
 }
