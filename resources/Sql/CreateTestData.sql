d2a9760a05fc515b5479d80683ee63cd14a395db
lukashecke <57808594+lukashecke@users.noreply.github.com>
2022-04-04 17:15:47 +0200

    Mehr Testdaten einfügen

--- resources/Sql/CreateTestData.sql
+++ resources/Sql/CreateTestData.sql
@@ -28,7 +28,10 @@ INSERT INTO Type (Id, NAME, ShortName, NeedsIncomeTax)
 VALUES (2, 'Kryptowährung', 'Krypto', 1);
 
 INSERT INTO Type (Id, NAME, ShortName, NeedsIncomeTax)
-VALUES (3, 'Edelmetalle', 'MET', 1);
+VALUES (3, 'Edelmetalle', 'Met.', 1);
+
+INSERT INTO Type (Id, NAME, ShortName, NeedsIncomeTax)
+VALUES (4, 'Einzelaktien', 'Aktien', 1);
 
 -- -----------------------------------------------------
 -- Assets
@@ -44,6 +47,45 @@ VALUES (3, 2, 'Ethereum', 'ETH');
 
 INSERT INTO asset (Id, Type_Id, Name, ShortName)
 VALUES (4, 3, 'Gold', 'GLD');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (5, 4, 'Tesla', 'TSLA');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (6, 4, 'Microsoft', 'MSFT');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (7, 4, 'Apple', 'AAPL');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (8, 2, 'Polkadot', 'DOT');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (9, 2, 'Solana', 'SOL');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (10, 2, 'Cardano', 'ADA');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (11, 4, 'Nike', 'NKE');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (12, 1, 'Digital Security EUR', 'DIGS');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (13, 1, 'Global Clean Energy EUR', 'GCE');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (14, 1, 'S&P500 EUR', 'S&P');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (15, 3, 'Silber', 'SLB');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (16, 3, 'Titan', 'TITN');
+
+INSERT INTO asset (Id, Type_Id, Name, ShortName)
+VALUES (17, 3, 'Diamanten', 'DMNDS');
 
 -- -----------------------------------------------------
 -- Labels
@@ -78,6 +120,18 @@ VALUES (5, 3, DATE('2022-03-15 00:00:00'), 2405.73);
 INSERT INTO history (Id, Asset_Id, TimeStamp, PricePerUnit)
 VALUES (6, 4, DATE('2022-03-30 00:00:00'), 50.00);
 
+INSERT INTO history (Id, Asset_Id, TimeStamp, PricePerUnit)
+VALUES (7, 5, DATE('2022-03-30 00:00:00'), 1020.60);
+
+INSERT INTO history (Id, Asset_Id, TimeStamp, PricePerUnit)
+VALUES (8, 5, DATE('2022-03-20 00:00:00'), 1010.60);
+
+INSERT INTO history (Id, Asset_Id, TimeStamp, PricePerUnit)
+VALUES (9, 4, DATE('2022-03-20 00:00:00'), 177.72);
+
+INSERT INTO history (Id, Asset_Id, TimeStamp, PricePerUnit)
+VALUES (10, 14, DATE('2022-03-20 00:00:00'), 4565.69);
+
 -- -----------------------------------------------------
 -- Investments
 -- -----------------------------------------------------
@@ -98,6 +152,18 @@ VALUES (5, 1, 2, 3, 5, 50.00, 1.00);
 
 INSERT INTO investment (Id, Portfolio_Id, Platform_Id, Asset_Id, History_Id, PurchasePrice, TransactionFee)
 VALUES (6, 1, 3, 4, 6, 50.00, 1.00);
+
+INSERT INTO investment (Id, Portfolio_Id, Platform_Id, Asset_Id, History_Id, PurchasePrice, TransactionFee)
+VALUES (7, 1, 1, 5, 7, 50.00, 1.00);
+
+INSERT INTO investment (Id, Portfolio_Id, Platform_Id, Asset_Id, History_Id, PurchasePrice, TransactionFee)
+VALUES (8, 1, 1, 5, 8, 50.00, 1.00);
+
+INSERT INTO investment (Id, Portfolio_Id, Platform_Id, Asset_Id, History_Id, PurchasePrice, TransactionFee)
+VALUES (9, 1, 1, 4, 9, 50.00, 1.00);
+
+INSERT INTO investment (Id, Portfolio_Id, Platform_Id, Asset_Id, History_Id, PurchasePrice, TransactionFee)
+VALUES (10, 1, 1, 14, 10, 75.00, 1.00);
 
 -- -----------------------------------------------------
 -- Benutzer und Berechtigungen
